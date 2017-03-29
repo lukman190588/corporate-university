79fe3312d899a74f6bb208536adc86e269e0d5cb
raeyjuve <raey_juve@yahoo.com>
2017-03-29 10:42:02 +0700

    revert sleep to random

--- corpu-backend/src/main/java/com/id/corpu/backend/controller/EventController.java
+++ corpu-backend/src/main/java/com/id/corpu/backend/controller/EventController.java
@@ -61,8 +61,7 @@ public class EventController {
 	    	TreeMap<String, Object> tResp = new TreeMap<String, Object>();
 	    	tResp.put("items", listPaged.get(pageNumber-1));
 	    	tResp.put("total", list.size());
-//	    	Thread.sleep(new Random().nextInt((2000 - 500) + 1) + 500);
-	    	Thread.sleep(10000);
+	    	Thread.sleep(new Random().nextInt((2000 - 500) + 1) + 500);
 	    	return tResp;
 		} catch (Exception e) {
 			e.printStackTrace();
